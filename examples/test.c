5bbdcd3fca684872cc41203f000fe08dea7f220d
Patrick Chilton <chpatrick@gmail.com>
2015-06-21 15:17:17 +0100

    Turn test.hs into goto counter.

--- examples/test.c
+++ examples/test.c
@@ -1,5 +1,7 @@
 #include "stdio.h"
 
 void foo() {
+  lol:
   printf("%d", (2 + 2) + (2 * 2));
+  goto lol;
 }
--- examples/test.hs
+++ examples/test.hs
@@ -1,20 +1,27 @@
-{-# LANGUAGE PatternSynonyms, ViewPatterns #-}
+{-# LANGUAGE PatternSynonyms, ViewPatterns, RankNTypes, LambdaCase #-}
 
 import Clang
+import Data.Function
 import Control.Lens
 import qualified Data.ByteString.Char8 as BS
+import Data.Monoid
 import Data.Tree
+
+-- generalized universeOf
+univ :: Fold a a -> Fold a a
+univ fld f = fld $ \x -> f x *> univ fld f x
 
 main = do
   idx <- createIndex
   tu <- parseTranslationUnit idx "examples/test.c" []
   let
     Just srcFile = getFile tu "examples/test.c"
-    loc
-      = fmap (file . spellingLocation . rangeStart) . cursorExtent
-    ast
-      = flip unfoldTree (translationUnitCursor tu) $ \n ->
-        ( show $ cursorKind n
-        , toListOf (droppingWhile (\c -> loc c /= Just srcFile) cursorChildren) n
-        )
-  putStrLn (drawTree ast)
+    inFile c = case cursorExtent c of
+      Nothing -> False
+      Just ex -> (file $ spellingLocation $ rangeStart ex) == srcFile
+    root = translationUnitCursor tu
+    allNodes = univ (cursorChildren . filtered inFile)
+    gotos = lengthOf (allNodes . filtered (\c -> cursorKind c == GotoStmt)) root
+  putStrLn $ if gotos == 0
+    then "No gotos!"
+    else show gotos ++ " goto(s)! You should feel bad!"
