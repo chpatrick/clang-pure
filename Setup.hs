d39613dbfc2eda8e118b5cdb51313fa0012b1120
Patrick Chilton <chpatrick@gmail.com>
2016-08-23 01:36:20 +0100

    Make Setup.hs style consistent with the rest of the code.

--- Setup.hs
+++ Setup.hs
@@ -1,7 +1,7 @@
 {-|
 Module      : Main
 Description : Setup script for clang-pure build
-Copyright   : (C) Richard Cook, 2016
+Copyright   : (C) Richard Cook, Patrick Chilton 2016
 Licence     : Apache 2.0
 Maintainer  : rcook@rcook.org
 Stability   : experimental
@@ -9,8 +9,8 @@ Portability : portable
 
 This setup script adds a configuration hook to the build process to discover
 the LLVM library and include paths from the standard system-wide installation
-location or from an alternative location if overridden with the
-@CLANG_PURE_LLVM_DIR@ environment variable.
+location or from an alternative location if overridden with both the
+@CLANG_PURE_LLVM_LIB_DIR@ and @CLANG_PURE_LLVM_INCLUDE_DIR@ environment variables.
 -}
 
 {-# LANGUAGE CPP #-}
@@ -34,9 +34,9 @@ data SetupException = SetupException String deriving Show
 instance Exception SetupException
 
 data LLVMPathInfo = LLVMPathInfo
-    { llvmLibraryDir :: FilePath
-    , llvmIncludeDir :: FilePath
-    }
+  { llvmLibraryDir :: FilePath
+  , llvmIncludeDir :: FilePath
+  } deriving (Eq, Ord, Show)
 
 llvmLibDirEnvVarName :: String
 llvmLibDirEnvVarName = "CLANG_PURE_LLVM_LIB_DIR"
@@ -46,54 +46,55 @@ llvmIncludeDirEnvVarName = "CLANG_PURE_LLVM_INCLUDE_DIR"
 
 findLLVMConfigPaths :: IO LLVMPathInfo
 findLLVMConfigPaths = do
-    let llvmConfigCandidates =
-            "llvm-config" : [ "llvm-config-" ++ show major ++ "." ++ show minor
-                            | major <- [9,8..3 :: Int]
-                            , minor <- [9,8..0 :: Int]
-                            ]
-    let tryCandidates [] = throwIO $ SetupException "Could not find llvm-config."
-        tryCandidates (llvmConfig : candidates) = do
-            llvmConfigResult <- tryJust (guard . isDoesNotExistError) (readProcess llvmConfig ["--libdir", "--includedir"] "")
-            case llvmConfigResult of
-                Left _ -> tryCandidates candidates
-                Right llvmConfigOutput -> case lines llvmConfigOutput of
-                    [ libraryDir, includeDir ] -> return $ LLVMPathInfo libraryDir includeDir
-                    _ -> throwIO $ SetupException "Unexpected llvm-config output."
-    tryCandidates llvmConfigCandidates
+  let llvmConfigCandidates =
+        "llvm-config" :
+          [ "llvm-config-" ++ show major ++ "." ++ show minor
+          | major <- [9,8..3 :: Int]
+          , minor <- [9,8..0 :: Int]
+          ]
+  let tryCandidates [] = throwIO $ SetupException "Could not find llvm-config."
+      tryCandidates (llvmConfig : candidates) = do
+        llvmConfigResult <- tryJust (guard . isDoesNotExistError) (readProcess llvmConfig ["--libdir", "--includedir"] "")
+        case llvmConfigResult of
+          Left _ -> tryCandidates candidates
+          Right llvmConfigOutput -> case lines llvmConfigOutput of
+            [ libraryDir, includeDir ] -> return $ LLVMPathInfo libraryDir includeDir
+            _ -> throwIO $ SetupException "Unexpected llvm-config output."
+  tryCandidates llvmConfigCandidates
 
 getLLVMPathInfo :: IO LLVMPathInfo
 getLLVMPathInfo = do
-    m'llvmLibDirEnvVar <- lookupEnv llvmLibDirEnvVarName
-    m'llvmIncludeDirEnvVar <- lookupEnv llvmIncludeDirEnvVarName
-    case (m'llvmLibDirEnvVar, m'llvmIncludeDirEnvVar) of
-        ( Just libraryDir, Just includeDir ) -> return $ LLVMPathInfo libraryDir includeDir
-        _ -> findLLVMConfigPaths
+  m'llvmLibDirEnvVar <- lookupEnv llvmLibDirEnvVarName
+  m'llvmIncludeDirEnvVar <- lookupEnv llvmIncludeDirEnvVarName
+  case (m'llvmLibDirEnvVar, m'llvmIncludeDirEnvVar) of
+    ( Just libraryDir, Just includeDir ) -> return $ LLVMPathInfo libraryDir includeDir
+    _ -> findLLVMConfigPaths
 
 clangPureConfHook :: (GenericPackageDescription, HookedBuildInfo) -> ConfigFlags -> IO LocalBuildInfo
 clangPureConfHook (d, bi) flags = do
-    localBuildInfo <- confHook simpleUserHooks (d, bi) flags
-    let pd = localPkgDescr localBuildInfo
-        Just lib = library pd
-        lbi = libBuildInfo lib
+  localBuildInfo <- confHook simpleUserHooks (d, bi) flags
+  let pd = localPkgDescr localBuildInfo
+  let Just lib = library pd
+  let lbi = libBuildInfo lib
 
-    LLVMPathInfo{..} <- getLLVMPathInfo
+  LLVMPathInfo{..} <- getLLVMPathInfo
 
-    return localBuildInfo {
-        localPkgDescr = pd {
-            library = Just $ lib {
-                libBuildInfo = lbi
-                { includeDirs = llvmIncludeDir : includeDirs lbi
-                , extraLibDirs = llvmLibraryDir : extraLibDirs lbi
-                , cSources =
+  return localBuildInfo {
+    localPkgDescr = pd {
+      library = Just $ lib {
+        libBuildInfo = lbi
+        { includeDirs = llvmIncludeDir : includeDirs lbi
+        , extraLibDirs = llvmLibraryDir : extraLibDirs lbi
+        , cSources = -- define the generated c-sources here so that they don't get picked up by sdist
 #ifdef mingw32_HOST_OS
-                      ["srcLanguageCClangInternalFFI.c"] -- work around a bug in inline-c (?)
+            ["srcLanguageCClangInternalFFI.c"] -- work around a bug in inline-c (?)
 #else
-                      ["src/Language/C/Clang/Internal/FFI.c"]
+            ["src/Language/C/Clang/Internal/FFI.c"]
 #endif
-                }
-            }
         }
+      }
     }
+  }
 
 main :: IO ()
 main = defaultMainWithHooks simpleUserHooks { confHook = clangPureConfHook }
