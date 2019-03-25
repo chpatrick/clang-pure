38bd24743f1bc6156ab6ab467a4aab0a2e611bee
Patrick Chilton <chpatrick@gmail.com>
2019-03-25 19:06:42 +0100

    Bump version.

--- default.nix
+++ default.nix
@@ -4,31 +4,9 @@ let
 
   inherit (nixpkgs) pkgs;
 
-  haskellPackagesRaw = if compiler == "default"
-                       then pkgs.haskellPackages
-                       else pkgs.haskell.packages.${compiler};
-
-  haskellPackages = haskellPackagesRaw.override {
-    overrides = self: super: {
-      inline-c = let
-        srcRaw = pkgs.fetchFromGitHub
-          {
-            owner = "fpco";
-            repo = "inline-c";
-            rev = "8bae45eb7c308023965a6f069170b31b1acf4321";
-            sha256 = "0scsgjdpv44cvfgm6h5ydjkwkdjrm2j21nckif19zhkx2lvjbq4l";
-          };
-
-        src = pkgs.stdenv.mkDerivation {
-          name = "inline-c-src";
-          buildCommand = ''
-            cp -rv ${srcRaw}/inline-c $out
-          '';
-        };
-
-       in super.callCabal2nix "inline-c" src {};
-    };
-  };
+  haskellPackages = if compiler == "default"
+                    then pkgs.haskellPackages
+                    else pkgs.haskell.packages.${compiler};
 
   variant = if doBenchmark then pkgs.haskell.lib.doBenchmark else pkgs.lib.id;
 
